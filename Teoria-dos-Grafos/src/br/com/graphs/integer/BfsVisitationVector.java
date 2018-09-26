20b1ec0dfd0417ddfcf0edd40e13319837bb0359
Kimberly Lima <kimberlyplima@hotmail.com>
2018-09-26 09:51:42 -0300

    adding comments

--- Teoria-dos-Grafos/src/br/com/graphs/integer/BfsVisitationVector.java
+++ Teoria-dos-Grafos/src/br/com/graphs/integer/BfsVisitationVector.java
@@ -1,9 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a simple implementation of the BFS search and it returns a 
+ * visitation vector.
  */
-package br.com.teoria.dos.grafos;
+package br.com.graphs.integer;
 
 import java.util.*;
 
--- Teoria-dos-Grafos/src/br/com/graphs/integer/Graph.java
+++ Teoria-dos-Grafos/src/br/com/graphs/integer/Graph.java
@@ -1,7 +1,7 @@
 /*
  * This is an implementation of a simple graph of integers.
  */
-package br.com.teoria.dos.grafos;
+package br.com.graphs.integer;
 
 import java.util.*;
 
@@ -16,6 +16,17 @@ public class Graph {
         graph = new HashMap();
     }
     
+    /*
+    * This method checks if the initial node already exists on the map.
+    *
+    * If the existing maps don't contain the node, a new map is created
+    * adding the inicial given node as the key of the map 
+    * and a null linkedlist as the value. It also adds the final node as an
+    * adjacency of the inicial node.
+    *
+    * If the map contains the inicial given node, the final node is added as an 
+    * adjacency of it (it's added on the linkedlist).
+    */
     public void buildGraph(Integer inicialVertex, Integer finalVertex){
         if(!graph.containsKey(inicialVertex)){
             graph.put(inicialVertex, new LinkedList());
@@ -23,6 +34,9 @@ public class Graph {
         } else graph.get(inicialVertex).add(finalVertex);
     }
     
+    /*
+    * Prints the entire map.
+    */
     public void showGraph(){
         Iterator<Map.Entry<Integer, LinkedList<Integer>>> i;
         i = graph.entrySet().iterator();
@@ -32,12 +46,15 @@ public class Graph {
         }
     }
     
+    /*
+    * @return an integer containing the number of keys in this map.
+    */
     public Integer sizeGraph(){
         return graph.size();
     }
 
     /**
-     * @return the graph
+     * @return the adjacencies of the given node.
      */
     public LinkedList<Integer> getAdjList(Integer vertex) {
         if(graph.get(vertex).contains(null)){
